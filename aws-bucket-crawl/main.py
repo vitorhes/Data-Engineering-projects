a5fa957557f197cfb396f5f9e9ceb98d47943be7
vitorhes <vitorhes@outlook.com>
2022-06-19 19:03:08 -0300

    logging and try blocks added, readme updated

--- aws-bucket-crawl/main.py
+++ aws-bucket-crawl/main.py
@@ -1,37 +1,47 @@
 import boto3
-from io import TextIOWrapper
+from io import TextIOWrapper, StringIO
 from gzip import GzipFile
-from io import StringIO
+import logging
 
+logging.basicConfig(filename = 'files.log', level = logging.INFO, 
+                format = "%(asctime)s:%(levelname)s:%(message)s")
 
-s3 = boto3.client('s3', region_name='eu-central-1') 
+def download_stream_file(bucket: str, key: str, s3) -> str:
+    """
+    fetch a gz file and stream it from a public S3 bucket
+    """
 
-def download_stream_file(bucket,key,sequence):
-
+    try:
     # get StreamingBody from botocore.response
-    response = s3.get_object(Bucket=bucket, Key=key)
-    # if gzipped
-    gzipped = GzipFile(None, 'rb', fileobj=response['Body'])
-    data = TextIOWrapper(gzipped)
 
-    if sequence == 1:
-        text_stream = StringIO()
-        for line in data:
-            text_stream.write(line)
-        url = text_stream.getvalue().split('\n', 1)[0].rstrip()
-        return url
+        response = s3.get_object(Bucket=bucket, Key=key)
+        object = GzipFile(None, 'rb', fileobj=response['Body'])
+        data = TextIOWrapper(object)
 
-    else:
-        for line in data:
-            print(line)
+    except Exception as e:
+        logging.error(e)
+
+    return data
 
 def main():
 
+    s3_conn = boto3.client('s3', region_name='eu-central-1') 
     BUCKET_NAME = "commoncrawl"
     FILE_KEY = "crawl-data/CC-MAIN-2022-05/wet.paths.gz"
 
-    url = download_stream_file(BUCKET_NAME,FILE_KEY,1)
-    download_stream_file(BUCKET_NAME,url,2)
+    #download the file in memory to fetch the url located in the first line of the file
+    text_stream = StringIO()
+
+    for line in download_stream_file(BUCKET_NAME,FILE_KEY,s3_conn):
+        text_stream.write(line)
+    url = text_stream.getvalue().split('\n', 1)[0].rstrip()
+    logging.info(f"URL fetched: {url}")
+
+    #use the url fetched above to download the second file, then stream and print it line by line
+    for line in download_stream_file(BUCKET_NAME,url,s3_conn):
+        print(line)
+    logging.info(f"Finished printing the file")
+
  
 if __name__ == '__main__':
     main()
