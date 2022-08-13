9b7be092f99f02e71fa4f81d166258c1e29a6e15
vitorhes <vitorhes@outlook.com>
2022-08-13 02:03:53 -0300

    stable version

--- simple-pipeline/createstatements.py
+++ simple-pipeline/createstatements.py
@@ -1,3 +1,26 @@
+tables_names_list = ["products", "accounts", "transactions"]
+
+
+
+accounts_drop_table = ("""
+ DROP TABLE IF EXISTS accounts;
+""")
+accounts_table_create = ("""
+    CREATE TABLE IF NOT EXISTS accounts
+        (
+            customer_id INTEGER PRIMARY KEY,
+            first_name VARCHAR,
+            last_name VARCHAR,
+            address_1 VARCHAR,
+            address_2 VARCHAR,  
+            city VARCHAR,
+            state VARCHAR,
+            zip_code VARCHAR,
+            join_date VARCHAR
+            
+        );
+""")
+
 
 products_drop_table = ("""
  DROP TABLE IF EXISTS products;
@@ -8,10 +31,27 @@ products_table_create = ("""
             product_id INTEGER PRIMARY KEY,
             product_code INTEGER NOT NULL,
             product_description VARCHAR
-            
         );
 """)
 
-drop_tables_list = [products_drop_table]
-create_table_list = [products_table_create]
-tables_names_list = ["products"]
+
+transactions_drop_table = ("""
+ DROP TABLE IF EXISTS transactions;
+""")
+
+transactions_table_create = ("""
+    CREATE TABLE IF NOT EXISTS transactions
+        (
+            transaction_id VARCHAR PRIMARY KEY,
+            transaction_date VARCHAR,
+            product_id INTEGER,
+            product_code INTEGER,
+            product_description VARCHAR,  
+            quantity INTEGER,
+            account_id INTEGER
+        );
+""")
+
+
+drop_tables_list = [products_drop_table,accounts_drop_table,transactions_drop_table]
+create_table_list = [products_table_create,accounts_table_create,transactions_table_create]
--- simple-pipeline/main.py
+++ simple-pipeline/main.py
@@ -2,6 +2,8 @@ import psycopg2
 from asyncio import create_task
 from createstatements import *
 import os
+
+
 def create_connection():
     host = 'postgres'
     database = 'postgres'
@@ -10,65 +12,50 @@ def create_connection():
     conn = psycopg2.connect(host=host, database=database, user=user, password=pas)
     cur = conn.cursor()
     conn.set_session(autocommit = True)
-    print("Sucessfuly connected to the databse")
+    print("-------Sucessfuly connected to the database-------")
     return cur, conn
 
 def create_tables(cur):
     '''
-    Create table based on the ddl statement
+    Drop every table, then recreate them based the ddl statement
     '''
     for drop_table in drop_tables_list:
         cur.execute(drop_table)
 
     for create_table in create_table_list:
         cur.execute(create_table)
-        print("Table created")
 
-def print_schema(cur):  
-    '''
-    Print the schema of the tables created. To make sure it`s correct
-    '''
 
-    print("----------------SCHEMA----------------")
+def data_ingestion(cur):
+    '''
+    Ingest data from csv files located in data folder
+    '''
     for table in tables_names_list:
-        sql_print_table = "SELECT * FROM "+table+" LIMIT 0"
-        cur.execute(sql_print_table)
-        colnames = [desc[0] for desc in cur.description]
-        
-        print(colnames)
-
-def ingestion(cur):
-    tables_list = ["products"]
-    for table in tables_list:
         current = os.getcwd()
         datafolder = f"data/{table}.csv"
         filepath = os.path.join (current, datafolder)
-    
 
         sql = f"COPY {table} FROM STDIN DELIMITER ',' CSV HEADER"
         cur.copy_expert(sql, open(filepath, "r"))
 
 def tester (cur):
     '''
-    Query data from a table an print it, to make sure it was loaded properly
+    Query data from a table and print it, to test if it was loaded properly
     '''
-
-    print("-----Testing: querying data from tables-----")
-    cur.execute("SELECT * FROM products")
-    row = cur.fetchone()
-    while row:
-        print(row)
-        row = cur.fetchmany()
+    for table in tables_names_list:
+        print(f"-----Testing: querying data from {table}-----")
+        cur.execute(f"SELECT * FROM {table}")
+        row = cur.fetchall()
+        while row:
+            print(row)
+            row = cur.fetchmany()
     print("Done testing")
 
 def main():
     cur, conn = create_connection()
     create_tables(cur)
-    print_schema(cur)
-    ingestion(cur)
+    data_ingestion(cur)
     tester(cur)
-
-
 
 
 if __name__ == '__main__':
